40732b9809b5bcb43bb14b5a1a921dc7bfe5f9f1
Tantely Hevitriniavo <tantely@vatilab.com>
2025-07-13 10:50:06 +0300

    VITE_API_URL vers VITE_BACKEND_URL

--- src/config/axiosClient.js
+++ src/config/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import useAuth from '~/hooks/useAuth'  
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000'
 
 const axiosClient = axios.create({
   baseURL: BASE_URL
